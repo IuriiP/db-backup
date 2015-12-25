180ab3fc7fd92a2806a676f409342aa995938e31
mouse <mouse@Asus>
2015-12-25 11:49:42 +0200

    Pre-production 17.1.2

--- src/config/config.php
+++ src/config/config.php
@@ -1,19 +1,14 @@
 <?php
-
-
 return [
 
-	'path' => storage_path() . '/dumps/',
-
-	'mysql' => [
-		'dump_command_path' => '',
-		'restore_command_path' => '',
-	],
-
-	's3' => [
-		'path' => ''
-	],
-
+    'path' => storage_path() . '/dumps/',
+    'mysql' => [
+        'dump_command_path' => '',
+        'restore_command_path' => '',
+    ],
+    's3' => [
+        'path' => ''
+    ],
     'compress' => false,
 ];
 
